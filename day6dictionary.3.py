0b3365d577939f8cf1b2fc5f70b997f3b8638685
dharunvishnu <dharunvishnu2006@gmail.com>
2026-04-07 17:10:40 +0530

    Day 7 - Dictionaries

---
+++ day6dictionary.3.py
@@ -0,0 +1,11 @@
+my_list ={
+    "name":"Jaganathan",
+    "age":48,
+    "city":"Erode",
+    "son":"Dharun"
+}
+print(my_list.keys())
+print(my_list.values())
+my_list["son_dream"]="CISO"
+
+print(my_list)
