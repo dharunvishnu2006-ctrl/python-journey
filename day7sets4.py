bd628d20a4f14970d8fd3ae6297614f110ff1efc
dharunvishnu <dharunvishnu2006@gmail.com>
2026-04-08 15:58:06 +0530

    Day 8 - Sets

---
+++ day7sets4.py
@@ -0,0 +1,6 @@
+students_A = {"dharun","ram","kumar","bala"}
+students_B = {"bala","senthil","dharun","arjun"}
+
+print(students_A | students_B)
+print(students_A & students_B)
+print(students_A - students_B)
