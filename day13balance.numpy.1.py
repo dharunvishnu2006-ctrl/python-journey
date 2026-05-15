9486acba052bb910e613c6c446d57a087a5a33b3
dharunvishnu <dharunvishnu2006@gmail.com>
2026-05-15 11:57:50 +0530

    Day 13 Step 36 NumPy Arrays Complete

---
+++ day13balance.numpy.1.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+zeros = np.zeros(5)
+ones = np.ones(5)
+range_arr = np.arange(0,10,2)
+
+print(zeros)
+print(ones)
+print(range_arr)
+
+bp = np.array([120,145,118,160,135])
+print("Mean:", np.mean(bp))
+print("Max:", np.max(bp))
+print("Min:", np.min(bp))
